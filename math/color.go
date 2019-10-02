4968601f6651ccc0bdb435d46448374f110e182d
Jean Catanho <jensmcatanho@gmail.com>
2019-10-02 01:45:17 -0300

    math: add new color operations

--- math/color.go
+++ math/color.go
@@ -1,15 +1,52 @@
 package math
 
+import stdMath "math"
+
 // Color is a structure that represents an RGB color
 type Color struct {
-	r, g, b float32
+	r, g, b float64
 }
 
 // NewColor creates a Color structure
-func NewColor(r, g, b float32) *Color {
+func NewColor(r, g, b float64) *Color {
 	return &Color{
 		r: r,
 		g: g,
 		b: b,
+	}
+}
+
+// Multiply multiplies a Color by a constant and returns a new Color with the result
+func (c *Color) Multiply(k float64) *Color {
+	return &Color{
+		r: c.r * k,
+		g: c.g * k,
+		b: c.b * k,
+	}
+}
+
+func (c *Color) Pow(k float64) *Color {
+	return &Color{
+		r: stdMath.Pow(c.r, k),
+		g: stdMath.Pow(c.g, k),
+		b: stdMath.Pow(c.b, k),
+	}
+}
+
+func (c *Color) ClampToColor(target Color) {
+	if c.r > 1.0 || c.g > 1.0 || c.b > 1.0 {
+		c.r = target.r
+		c.g = target.g
+		c.b = target.b
+	}
+}
+
+func (c *Color) MaxToOne() {
+	max := stdMath.Max(c.r, stdMath.Max(c.b, c.g))
+
+	if max > 1.0 {
+		c.r /= max
+		c.g /= max
+		c.b /= max
 	}
 }
--- math/color_test.go
+++ math/color_test.go
@@ -8,7 +8,25 @@ import (
 
 func Test_NewColor_WhenANewColorIsCreated(t *testing.T) {
 	color := NewColor(1., .5, .3)
-	assert.Equal(t, float32(1.), color.r)
-	assert.Equal(t, float32(.5), color.g)
-	assert.Equal(t, float32(.3), color.b)
+	assert.Equal(t, 1., color.r)
+	assert.Equal(t, .5, color.g)
+	assert.Equal(t, .3, color.b)
+}
+
+func Test_Color_Multiply_WhenSecondOperandIsPositive(t *testing.T) {
+	v := NewColor(1., 2., 3.)
+	result := v.Multiply(5.)
+
+	assert.Equal(t, 5., result.r)
+	assert.Equal(t, 10., result.g)
+	assert.Equal(t, 15., result.b)
+}
+
+func Test_Color_Multiply_WhenSecondOperandIsNegative(t *testing.T) {
+	v := NewColor(1., 2., 3.)
+	result := v.Multiply(-1.)
+
+	assert.Equal(t, -1., result.r)
+	assert.Equal(t, -2., result.g)
+	assert.Equal(t, -3., result.b)
 }
