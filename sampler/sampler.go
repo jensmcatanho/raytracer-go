eacc6a61a54c8b94a49510d0eefe4fdc2b7c1cd9
Jean Catanho <jensmcatanho@gmail.com>
2019-10-07 22:54:31 -0300

    sampler: move sampling methods to a separate file

--- sampler/sampler.go
+++ sampler/sampler.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"fmt"
 	"jensmcatanho/raytracer-go/math"
-	stdMath "math"
 	"math/rand"
 	"time"
 )
@@ -96,56 +95,5 @@ func (s *Sampler) setJump() {
 
 	if s.count%s.Samples == 0 {
 		s.jump = (rand.Int() % s.Sets) * s.Samples
-	}
-}
-
-func Regular(numSamples, numSets int, samples *[]math.Vector) {
-	n := int(stdMath.Sqrt(float64(numSamples)))
-
-	for i := 0; i < numSets; i++ {
-		for j := 0; j < n; j++ {
-			for k := 0; k < n; k++ {
-				x := (float64(k) + 0.5) / float64(n)
-				y := (float64(j) + 0.5) / float64(n)
-
-				*samples = append(*samples, *math.NewVector(float64(x), float64(y), 0.))
-			}
-		}
-	}
-}
-
-func Random(numSamples, numSets int, samples *[]math.Vector) {
-	rand.Seed(time.Now().UnixNano())
-
-	for i := 0; i < numSets; i++ {
-		for j := 0; j < numSamples; j++ {
-			*samples = append(*samples, *math.NewVector(rand.Float64(), rand.Float64(), .0))
-		}
-	}
-}
-
-// Hammersley generates sample using Hammersley points
-// http://holger.dammertz.org/stuff/notes_HammersleyOnHemisphere.html
-func Hammersley(numSamples, numSets int, samples *[]math.Vector) {
-	phi := func(j int) float64 {
-		x := 0.
-		f := .5
-
-		for j > 0 {
-			x += f * float64(j%2)
-			j /= 2
-			f *= .5
-		}
-
-		return x
-	}
-
-	for i := 0; i < numSets; i++ {
-		for j := 0; j < numSamples; j++ {
-			x := float64(j) / float64(numSamples)
-			y := phi(j)
-
-			*samples = append(*samples, *math.NewVector(x, y, 0.))
-		}
 	}
 }
